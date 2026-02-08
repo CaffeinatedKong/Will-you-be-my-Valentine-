1dd01aec0f2fe43717d908bdf3e318f5e0bc8e77
“PotatoKong” <3070582K@student.gla.ac.uk>
2026-02-08 21:55:52 +0800

    added

--- script.js
+++ script.js
@@ -54,15 +54,13 @@
 })();
 */
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
+    "HA?",
+    "HAAAAAAAAAAAAAA??",
+    "Will buy u dessert",
+    "Ill come with sweet potatoes",
+    "Fine Ill buy u cheescake",
     "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
+    "Nevermind I going to ask Xian Nu instead",
     "Just kidding, say yes please! ❤️"
 ];
 
